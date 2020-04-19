a68f9d71b038e65ad94f8da669fc32e4014b8a63
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-19 21:14:19 +0530

    Test router for testing the routing

---
+++ server/src/main/java/magneto/db/routing/MagnetoRouter.java
@@ -0,0 +1,66 @@
+package magneto.db.routing;
+
+import magneto.db.node.Node;
+
+import java.util.Arrays;
+
+public class MagnetoRouter implements Node{
+  private final String idc;
+  private final String ip;
+  private final int port;
+
+  public MagnetoRouter(String idc,String ip, int port) {
+      this.idc = idc;
+      this.ip = ip;
+      this.port = port;
+  }
+
+  @Override
+  public String getKey() {
+      return idc + "-"+ip+":"+port;
+  }
+
+  @Override
+  public String toString(){
+      return getKey();
+  }
+
+  public static void main(String[] args) {
+      //initialize 4 service node
+      MagnetoRouter node1 = new MagnetoRouter("IDC1","127.0.0.1",8080);
+      MagnetoRouter node2 = new MagnetoRouter("IDC1","127.0.0.1",8081);
+      MagnetoRouter node3 = new MagnetoRouter("IDC1","127.0.0.1",8082);
+      MagnetoRouter node4 = new MagnetoRouter("IDC1","127.0.0.1",8084);
+
+      //hash them to hash ring
+      ConsistentHashing<MagnetoRouter> consistentHashing = new ConsistentHashing<>(Arrays.asList(node1,node2,node3,node4),10);
+
+
+      //we have 5 requester ip, we are trying them to route to one service node
+      String requestIP1 = "hello";
+      String requestIP2 = "world";
+      String requestIP3 = "test";
+      String requestIP4 = "heyhey";
+      String requestIP5 = "lol";
+
+      goRoute(consistentHashing,requestIP1,requestIP2,requestIP3,requestIP4,requestIP5);
+
+      MagnetoRouter node5 = new MagnetoRouter("IDC2","127.0.0.1",8080);//put new service online
+      System.out.println("-------------putting new node online " +node5.getKey()+"------------");
+      consistentHashing.addNode(node5,10);
+
+      goRoute(consistentHashing,requestIP1,requestIP2,requestIP3,requestIP4,requestIP5);
+
+      consistentHashing.removeNode(node3);
+      System.out.println("-------------remove node online " + node3.getKey() + "------------");
+      goRoute(consistentHashing,requestIP1,requestIP2,requestIP3,requestIP4,requestIP5);
+
+
+  }
+
+  private static void goRoute(ConsistentHashing<MagnetoRouter> consistentHashing ,String ... requestIps){
+      for (String requestIp: requestIps) {
+          System.out.println(requestIp + " is route to " + consistentHashing.routeNode(requestIp));
+      }
+  }
+}
