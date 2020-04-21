13ffd3350d962e746639042aa375920b8297b869
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-21 20:06:13 +0530

    Magneto exceptions

---
+++ server/src/main/java/magneto/db/exceptions/InvalidCommandException.java
@@ -0,0 +1,12 @@
+package magneto.db.exceptions;
+
+import magneto.db.exceptions.MagnetoException;
+
+public class InvalidCommandException extends MagnetoException {
+
+  private static final long serialVersionUID = 1L;
+
+  public InvalidCommandException() {
+    super();
+  }
+}
---
+++ server/src/main/java/magneto/db/exceptions/MagnetoException.java
@@ -0,0 +1,14 @@
+package magneto.db.exceptions;
+
+public class MagnetoException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public MagnetoException() {
+    super();
+  }
+  
+  public MagnetoException(String s) {
+    super(s);
+  }
+}
