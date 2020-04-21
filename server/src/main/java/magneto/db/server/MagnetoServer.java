38628467844d71971a1e815f13d8ecac6fd1c182
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-21 20:05:31 +0530

    Refractor magneto server to handle requests

--- server/src/main/java/magneto/db/server/MagnetoServer.java
+++ server/src/main/java/magneto/db/server/MagnetoServer.java
@@ -1,7 +1,8 @@
 package magneto.db.server;
 
+import magneto.db.requests.MagnetoRequests;
 import magneto.db.routing.MagnetoClientRouter;
-import magneto.db.store.*;
+import magneto.db.store.MagnetoStore;
 
 import java.io.IOException;
 
@@ -11,11 +12,8 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
-
-import javax.xml.crypto.Data;
 
 /**
  * @author Ashik Meerankutty
@@ -24,18 +22,22 @@ import javax.xml.crypto.Data;
 
 public class MagnetoServer {
 
+    public static final String CONN_EXIT = "exit";
     public Selector selector;
     public ServerSocketChannel magnetoSocket;
+    public MagnetoStore magnetoStore;
+    public MagnetoRequests requests;
 
     MagnetoServer(int port) throws IOException {
+        this.magnetoStore = new MagnetoStore();
+        this.requests = new MagnetoRequests();
         this.selector = Selector.open();
         this.magnetoSocket = ServerSocketChannel.open();
         InetSocketAddress magnetoAddress = new InetSocketAddress("localhost", port);
-        this.magnetoSocket.bind(magnetoAddress);
-        this.magnetoSocket.configureBlocking(false);
+        this.magnetoSocket.bind(magnetoAddress).configureBlocking(false);
         int ops = magnetoSocket.validOps();
         magnetoSocket.register(selector, ops, null);
-        System.out.println("Server started waiting for client connection at "+"localhost"+":"+port);
+        System.out.println("Server started waiting for client connection at " + "localhost" + ":" + port);
     }
 
     public static void main(String args[]) throws IOException {
@@ -43,27 +45,20 @@ public class MagnetoServer {
         int port = Integer.parseInt(args[1]);
 
         MagnetoServer magnetoServer = new MagnetoServer(port);
-        if(nodeType.equals("node"))
-        {
+        if (nodeType.equals("node")) {
             magnetoServer.runNode();
         }
-        if(nodeType.equals("master")) {
+        if (nodeType.equals("master")) {
             magnetoServer.runMaster();
         }
     }
 
     public void runNode() throws IOException {
-        MagnetoStore magnetoStore = new MagnetoStore();
-
         // Keeps server running
         while (true) {
             // Selects a set of keys whose corresponding channels are ready for I/O
             // operations
-            try {
-                this.selector.select();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            this.selector.select();
 
             Set<SelectionKey> magnetoKeys = selector.selectedKeys();
             Iterator<SelectionKey> magnetoIterator = magnetoKeys.iterator();
@@ -74,31 +69,18 @@ public class MagnetoServer {
                 // Tests whether this key's channel is ready to accept a new socket connection
                 if (selectedKey.isAcceptable()) {
                     SocketChannel magnetoClient = this.magnetoSocket.accept();
-                    magnetoClient.configureBlocking(false);
-                    magnetoClient.register(selector, SelectionKey.OP_READ);
+                    magnetoClient.configureBlocking(false).register(selector, SelectionKey.OP_READ);
                     System.out.println("Connection accepted: " + magnetoClient.getLocalAddress());
                 } else if (selectedKey.isReadable()) {
                     SocketChannel magnetoClient = (SocketChannel) selectedKey.channel();
                     ByteBuffer magnetoBuffer = ByteBuffer.allocate(256);
                     magnetoClient.read(magnetoBuffer);
                     String data = new String(magnetoBuffer.array()).trim();
-                    if(data.equals("-1")) {
+                    if (data.equals(CONN_EXIT)) {
+                        System.out.println("Connection disconnected: " + magnetoClient.getLocalAddress() );
                         magnetoClient.close();
-                    } 
-                    String[] words = new String[3];
-                    if (data.length() > 0) {
-                        words = data.split("\\s+");
-                        if (words[0].equals("put"))
-                            magnetoStore.addToStore(words[1], words[2]);
-                        if (words[0].equals("get")) {
-                            String response = magnetoStore.getFromStore(words[1]);
-                            byte[] message = new String(response).getBytes();
-                            ByteBuffer responseBuffer = ByteBuffer.wrap(message);
-                            magnetoClient.write(responseBuffer);
-                        }
                     }
-
-                    System.out.println("Message received: " + data);
+                    requests.handleRequest(data, magnetoClient, magnetoStore);
                 }
             }
             magnetoIterator.remove();
@@ -135,17 +117,17 @@ public class MagnetoServer {
                     ByteBuffer magnetoBuffer = ByteBuffer.allocate(256);
                     magnetoClient.read(magnetoBuffer);
                     String data = new String(magnetoBuffer.array()).trim();
-                    if(data.equals("-1")) {
+                    if (data.equals("-1")) {
                         magnetoClient.close();
-                    } 
+                    }
                     String[] words = new String[3];
                     words = data.split("\\s+");
                     String key = words[1];
                     String response;
-                    if(words[0].equals("put")) {
+                    if (words[0].equals("put")) {
                         response = magnetoClientRouter.getNode(key, data);
                     }
-                    if(words[0].equals("get")) {
+                    if (words[0].equals("get")) {
                         response = magnetoClientRouter.getNode(key, data);
                         byte[] message = new String(response).getBytes();
                         ByteBuffer responseBuffer = ByteBuffer.wrap(message);
